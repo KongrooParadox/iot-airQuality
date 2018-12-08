f31f1e25288a4f1dcfb03de5628cd3ae1facd39c
FrogGamer <nanty.guillaume@gmail.com>
2018-12-09 00:06:56 +0100

    Fixed exceptions on Interuption

--- sensor.py
+++ sensor.py
@@ -5,54 +5,56 @@ import subprocess
 import Adafruit_DHT as dht
 from datetime import datetime
 
-# Executer ce script en tant que super user
-
+DELAY = 10
 
 def init():
-	global pi 
-	pi = pigpio.pi()
-	if not pi.connected:
-		print('Erreur : démarrage de pigpiod')
-		start = 'pigpiod'
-		subprocess.check_call(start.split())
-		#exit()
-
-	global air 
-	air = pi.i2c_open(1, 0x5a)
-
-
-def run(sleep_time):
-	global air
-	while(1):
-		c, d = pi.i2c_read_device(air, 9)
-		h,t = dht.read_retry(dht.DHT22, 8)
-
-		tvoc = d[7] * 256 + d[8]
-		co2 = (d[0] * 256) + d[1] 
-		if(co2 > 2500):
-			print('Attention taux de CO2 dangereux !!!')
-		if(tvoc > 700):
-			print('Attention taux TVOC dangereux!!!')
-		
-		print('#### {:%d/%m/%Y %H:%M:%S} ####'.format(datetime.now()))
-		print('Température : {:.02f} °C'.format(t) )
-		print('Humidité : {:.02f} %'.format(h) )
-		print('CO2 (ppm) [450-2000] : %s' % co2)
-		print('TVOC (ppb) [125-600] : %s' % tvoc)
-		print('')
-		time.sleep(sleep_time)
-
+	print('############################################\n\tRunning sensor.py\n############################################')
+	start_command = 'sudo pigpiod'
+	subprocess.call(start_command.split())
+	time.sleep(1)	 
 
 def close():
-	global pi, air
-	pi.i2c_close(air)
-	pi.stop()
-
+	stop_command = 'sudo killall pigpiod'
+	subprocess.call(stop_command.split())
+	print('\n############################################\n\tExiting sensor.py\n############################################')
 
 def main():
-	init()
-	run(10)
-	close()
+	try:
+		init()
+		pi = pigpio.pi()
+		if not pi.connected:
+			print('Erreur au démarrage de pigpiod')
+			init()
+ 
+		air = pi.i2c_open(1, 0x5a)
+
+		while(1):
+			c, d = pi.i2c_read_device(air, 9)
+			h,t = dht.read_retry(dht.DHT22, 8)
+
+			tvoc = d[7] * 256 + d[8]
+			co2 = (d[0] * 256) + d[1] 
+			if(co2 > 2500):
+				print('Attention taux de CO2 dangereux !!!')
+			if(tvoc > 700):
+				print('Attention taux TVOC dangereux!!!')
+			
+			print('#### {:%d/%m/%Y %H:%M:%S} ####'.format(datetime.now()))
+			print('Température : {:.02f} °C'.format(t) )
+			print('Humidité : {:.02f} %'.format(h) )
+			print('CO2 (ppm) [450-2000] : %s' % co2)
+			print('TVOC (ppb) [125-600] : %s' % tvoc)
+			print('')
+			time.sleep(DELAY)
+
+	finally:
+		try:
+			pi.i2c_close(air)
+			pi.stop()
+			close()
+		
+		except:
+			close()
 
 if __name__ == '__main__':
 	main()
